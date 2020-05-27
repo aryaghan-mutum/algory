d469694c06925edf85340e482739e489d6488042
anurag <anurag.muthyam@rocketmail.com>
2020-05-26 22:07:42 -0400

    added description to methods in numerical computation files

--- src/algos/NumericalComputation.ts
+++ src/algos/NumericalComputation.ts
@@ -1,7 +1,7 @@
 /**
  * Algory
  *
- * Copyright (c) Anuragn Muthyam
+ * Copyright (c) Anurag Muthyam
  * https://github.com/aryaghan-mutum
  *
  * (MIT License)
@@ -27,112 +27,144 @@ const err = require('../TypeViolation')
 const curry = require('curry')
 
 /**
- *
- * @param n
+ * Square a number
+ *  F(n) => (* n n)
  */
 export const square = curry(
+    /**
+     * @param n
+     */
     (n: number): number => {
         err.numberTypeViolationError(n)
         return n * n
     })
 
 /**
- *
- * @param n
+ * Cube a number
+ * F(n) => (* n n n)
  */
 export const cube = curry(
+    /**
+     * @param n
+     */
     (n: number): number => {
         err.numberTypeViolationError(n)
         return square(n) * n
     })
 
-
 /**
- *
- * @param x
- * @param y
+ * Square two numbers and sum the returned values
+ * F(n) => (+ (square x) (square y))
  */
 export const sumOfSquares = curry(
+    /**
+     * @param x
+     * @param y
+     */
     (x: number, y: number): number => {
         return square(x) + square(y)
     })
 
 /**
- *
- * @param x
- * @param y
+ * Cube two numbers and sum the returned values
+ * F(n) => (+ (cube x) (cube y))
  */
 export const sumOfCubes = curry(
+    /**
+     * @param x
+     * @param y
+     */
     (x: number, y: number): number => {
         return cube(x) + cube(y)
     })
 
 /**
- *
- * @param n
+ * Double a number.
+ * F(n) => (* n 2) or F(n) => (+ n n)
  */
 export const double = curry(
+    /**
+     * @param n
+     */
     (n: number): number => {
         err.numberTypeViolationError(n)
         return n * 2
     })
 
 /**
- *
- * @param n
+ * Triple a number.
+ * F(n) => (* n 3) or F(n) => (+ n n n)
  */
 export const triple = curry(
+    /**
+     * @param n
+     */
     (n: number): number => {
         err.numberTypeViolationError(n)
         return n * 3
     })
 
 /**
- *
- * @param n
+ * Add a number
+ * F(n) => (+ n 1)
  */
 export const add1 = curry(
+    /**
+     * @param n
+     */
     (n: number): number => {
         err.numberTypeViolationError(n)
         return n + 1
     })
 
 /**
- *
- * @param n
+ * Subtract a number
+ * F(n) => (- n 1)
  */
 export const sub1 = curry(
+    /**
+     * @param n
+     */
     (n: number): number => {
         err.numberTypeViolationError(n)
         return n - 1
     })
 
 /**
- *
- * @param n
+ * Subtract two numbers
+ * F(n) => (- n 2)
  */
 export const sub2 = curry(
+    /**
+     * @param n
+     */
     (n: number): number => {
         err.numberTypeViolationError(n)
         return n - 2
     })
 
 /**
- *
- * @param a
- * @param b
+ * Get the minimum number between two numbers
+ * F(x, y) => (< a b) ? a : b
  */
 export const min = curry(
+    /**
+     * @param a
+     * @param b
+     */
     (a: number, b: number): number => {
         return a < b ? a : b
     })
 
 /**
- *
- * @param a
- * @param b
+ * Get the maximum number between two numbers
+ * F(x, y) => (> a b) ? a : b
  */
 export const max = curry(
+    /**
+     * @param a
+     * @param b
+     */
     (a: number, b: number): number => {
         return a > b ? a : b
     })
@@ -148,20 +180,26 @@ export const max = curry(
 // })
 
 /**
- *
- * @param n
+ * Reciprocal/Inverse a number
+ * F(n) => (/ 1 n)
  */
 export const reciprocal = curry(
+    /**
+     * @param n
+     */
     (n: number): number | string => {
         err.numberTypeViolationError(n)
         return numerPred.isZero(n) ? 'reciprocal of 0 is undefined' : 1 / n
     })
 
 /**
- *
- * @param n
+ * Get an Absolute/modulus a number
+ * F(n) => |n|
  */
 export const abs = curry(
+    /**
+     * @param n
+     */
     (n: number): number => {
         err.numberTypeViolationError(n)
         return numerPred.isPositive(n) ? n : -(n)
--- src/algos/NumericalComputationPredicates.ts
+++ src/algos/NumericalComputationPredicates.ts
@@ -27,104 +27,151 @@ const curry = require('curry')
 const err = require('../TypeViolation')
 
 /**
- *
- * @param n
+ * Return true if the "n" is 0, false otherwise
+ * F?(n) => (= n 0) ? true : false
  */
-export const isZero = curry((n: number): boolean => {
-    err.numberTypeViolationError(n)
-    return (n ==- 0) ? true : false
-})
+export const isZero = curry(
+    /**
+     * @param n
+     */
+    (n: number): boolean => {
+        err.numberTypeViolationError(n)
+        return (n === 0) ? true : false
+    })
 
 /**
- *
- * @param n
+ * Return true if the "n" is 1, false otherwise
+ * F?(n) => (= n 1) ? true : false
  */
-export const isOne = curry((n: number): boolean => {
-    err.numberTypeViolationError(n)
-    return (n === 1) ? true : false
-})
-
-
-/**
- *
- * @param n
- */
-export const isNegative = curry((n: number): boolean => {
-    err.numberTypeViolationError(n)
-    return (n < 0) ? true : false
-})
+export const isOne = curry(
+    /**
+     * @param n
+     */
+    (n: number): boolean => {
+        err.numberTypeViolationError(n)
+        return (n === 1) ? true : false
+    })
 
 /**
- *
- * @param n
+ * Return true if the "n" is negative, false otherwise
+ * F?(n) => (< n 0) ? true : false
  */
-export const isPositive = curry((n: number): boolean => {
-    err.numberTypeViolationError(n)
-    return (n >= 0) ? true : false
-})
+export const isNegative = curry(
+    /**
+     * @param n
+     */
+    (n: number): boolean => {
+        err.numberTypeViolationError(n)
+        return (n < 0) ? true : false
+    })
 
 /**
- *
- * @param n
+ * Return true if the "n" is positive, false otherwise
+ * F?(n) => (>= n 0) ? true : false
  */
-export const isLesserThanAndEqualToZero = curry((n: number): boolean => {
-    err.numberTypeViolationError(n)
-    return (n <= 0) ? true : false
-})
+export const isPositive = curry(
+    /**
+     * @param n
+     */
+    (n: number): boolean => {
+        err.numberTypeViolationError(n)
+        return (n >= 0) ? true : false
+    })
 
 /**
- * Analogous to the symbol "<"
- * @param x
- * @param y
+ * Return true if the "n" is lesser Than and equal to zero, false otherwise
+ * F?(n) => (<= n 0) ? true : false
  */
-export const lesserThan = curry((x: number, y: number): boolean => {
-    return x < y
-})
+export const isLesserThanAndEqualToZero = curry(
+    /**
+     * @param n
+     */
+    (n: number): boolean => {
+        err.numberTypeViolationError(n)
+        return (n <= 0) ? true : false
+    })
 
 /**
- * Analogous to the symbol ">"
- * @param x
- * @param y
+ * Return true if the "x" is lesser than "y", false otherwise
+ * lesserThan() is analogous to the operator "<"
+ * F?(n) => (< x y)
  */
-export const greaterThan = curry((x: number, y: number): boolean => {
-    return x > y
-})
+export const lesserThan = curry(
+    /**
+     * @param x
+     * @param y
+     */
+    (x: number, y: number): boolean => {
+        return x < y
+    })
 
 /**
- *
- * @param x
- * @param y
- * @param z
+ * Return true if the "x" is greater than "y", false otherwise
+ * greaterThan() is analogous to the operator ">"
+ * F?(n) => (> x y)
  */
-export const isSumLesser = curry((x: number, y: number, z: number): boolean => {
-    return (x + y) < z
-})
+export const greaterThan = curry(
+    /**
+     * @param x
+     * @param y
+     */
+    (x: number, y: number): boolean => {
+        return x > y
+    })
 
 /**
- *
- * @param x
- * @param y
- * @param z
+ * Return true if the sum of the returned value of first two formal parameter values
+ * is lesser than the third argument's value", false otherwise
+ * F?(n) => (< (+ x y) z)
  */
-export const isSumGreater = curry((x: number, y: number, z: number): boolean => {
-    return (x + y) > z
-})
+export const isSumLesser = curry(
+    /**
+     * @param x
+     * @param y
+     * @param z
+     */
+    (x: number, y: number, z: number): boolean => {
+        return (x + y) < z
+    })
 
 /**
- *
- * @param n
+ * Return true if the sum of the returned value of first two formal parameter values
+ * is greater than the third argument's value", false otherwise
+ * F?(n) => (> (+ x y) z)
  */
-export const isEven = curry((n: number): boolean => {
-    err.numberTypeViolationError(n)
-    return numer.abs(n) % 2 == 0 ? true : false
-})
+export const isSumGreater = curry(
+    /**
+     * @param x
+     * @param y
+     * @param z
+     */
+    (x: number, y: number, z: number): boolean => {
+        return (x + y) > z
+    })
 
 /**
- *
- * @param n
+ * Return true if the "n" is an even number
+ * F?(n) => (= (% (abs n) 2) 0) ? true : false
  */
-export const isOdd = curry((n: number): boolean => {
-    err.numberTypeViolationError(n)
-    return numer.abs(n) % 2 !== 0 ? true : false
-})
+export const isEven = curry(
+    /**
+     * @param n
+     */
+    (n: number): boolean => {
+        err.numberTypeViolationError(n)
+        return numer.abs(n) % 2 == 0 ? true : false
+    })
+
+/**
+ * Return true if the "n" is an odd number
+ * F?(n) => (!= (% (abs n) 2) 0) ? true : false
+ */
+export const isOdd = curry(
+    /**
+     * @param n
+     */
+    (n: number): boolean => {
+        err.numberTypeViolationError(n)
+        return numer.abs(n) % 2 !== 0 ? true : false
+    })
 
