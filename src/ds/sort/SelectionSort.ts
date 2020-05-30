715f6b2e81a1004ab6ccfac508008dfd4b776721
anurag <anurag.muthyam@rocketmail.com>
2020-05-30 17:20:00 -0400

    impl selection sort

--- src/ds/sort/SelectionSort.ts
+++ src/ds/sort/SelectionSort.ts
@@ -15,18 +15,16 @@ const curry = require('curry')
  */
 export const selectionSortImper = curry(
     (arr: Array<number>): Array<number> | null => {
-
+        debugger
         // if the array is null or undefined then return null
         if (arr === undefined || arr === null) return null
 
         // One by one move boundary of unsorted subarray
         for (let i = 0; i < arr.length; i++) {
-
             // Find the minimum element in unsorted array
             let min = i
 
             for (let j = i + 1; j < arr.length; j++) {
-
                 // if the elem in first index is greater than elem in second index the swap
                 if (arr[min] > arr[j]) {
                     min = j
