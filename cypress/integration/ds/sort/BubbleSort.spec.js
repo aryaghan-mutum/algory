770368ae0ffc948affb6db646d299a6ff8059bf4
anurag <anurag.muthyam@rocketmail.com>
2020-05-31 21:54:43 -0400

    refactored bubble

--- cypress/integration/ds/sort/BubbleSort.spec.js
+++ cypress/integration/ds/sort/BubbleSort.spec.js
@@ -11,5 +11,14 @@ describe('Bubble Sort', () => {
         expect(sort.bubbleSortImper(null)).deep.equal(null)
     })
 
+    specify('test bubbleSortFunc', () => {
+        expect(sort.bubbleSortFunc([-6, 8, -5, 0, -3, 4, 0, -5, 8, -3])).deep.equal([-6, -5, -5, -3, -3, 0, 0, 4, 8, 8])
+        expect(sort.bubbleSortFunc([-4, -3, -2, -1, 0, 1, 2, 3, 4])).deep.equal([-4, -3, -2, -1, 0, 1, 2, 3, 4])
+        expect(sort.bubbleSortFunc([4, 3, 2, 1, 0, -1, -2, -3, -4])).deep.equal([-4, -3, -2, -1, 0, 1, 2, 3, 4])
+        expect(sort.bubbleSortFunc([0])).deep.equal([0])
+        expect(sort.bubbleSortFunc([])).deep.equal([])
+        expect(sort.bubbleSortFunc(null)).deep.equal(null)
+    })
+
 })
 
